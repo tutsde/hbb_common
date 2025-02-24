7aada2be4b85eb4173f7ce13b175f9fd8372599f
Tobias Wack <tobias@tuts.de>
2025-02-24 16:48:29 +0100

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["141.2.163.10"];
+pub const RS_PUB_KEY: &str = "pcsQv++f66KDJ1YIsLhHKbMbqJpUhKSYlZk42XDUmqg=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
